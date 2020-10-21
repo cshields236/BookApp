c041540d22d86fdf3119f069a09a128b6270d541
Conor Shields <conor.shields1@mydit.ie>
2020-10-21 22:37:17 +0100

    getting just book titles

--- Shelf_Parser.py
+++ Shelf_Parser.py
@@ -2,13 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 
 
-response = requests.get("https://www.goodreads.com/review/list?v=2 &id=106016596&shelf=to-read&key=7MFYkvoWpEg6bVvA6GuLyQ")
+response = requests.get("https://www.goodreads.com/review/list?v=2 &id=106016596&shelf=to-read&key =7MFYkvoWpEg6bVvA6GuLyQ")
 
 html_file = response.text
 soup = BeautifulSoup(html_file, 'html.parser')
 soup.prettify()
-toRead = soup.findAll('td', 'field title', 'value')
-for book in toRead:
-    print(book.getText())
+toRead_title = soup.findAll('td', 'field title', 'title')
+for book in toRead_title:
+    if  book.getText() is not 'title':
+        print(book.getText().replace('title','').strip())
 
+
+# toRead_author =  soup.findAll('td', 'field author', 'title')
+# for author in toRead_author:
+#     print(author.getText())
+
+# for tag in soup.findAll('td', 'field title'):
+#     print(tag)
 
