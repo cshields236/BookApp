3d45a1404fdc9cf9e2282cf9d802d3a2dbc39264
Conor Shields <conor.shields1@mydit.ie>
2020-10-22 17:24:47 +0100

    adding covers to book object

--- Book.py
+++ Book.py
@@ -1,15 +1,12 @@
 class Book:
-    # TODO Add isbn, number of pages, cover image
+
     def __init__(self):
         type = 'book'
         
-    def addDetails(self, title, author, avg_rating, isbn ):
+    def addDetails(self, title, author, avg_rating, isbn, cover_image ):
         self.title = title
         self.author = author
         self.avg_rating = avg_rating
         self.isbn = isbn
+        self.cover_image = cover_image
 
-    def showBook(self):
-        print('Title ' + self.title + '\n'
-              + 'Author ' + self.author + '\n'
-              + 'Average Rating ' + self.avg_rating)
--- Shelf_Parser.py
+++ Shelf_Parser.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from Book import Book
+from random import randint
 
 response = requests.get("https://www.goodreads.com/review/list?v=2 &id=106016596&shelf=to-read&key =7MFYkvoWpEg6bVvA6GuLyQ")
 bookList =[]
@@ -8,28 +9,28 @@ titleArr =[]
 authorArr = []
 ratingArr = []
 coversArr = []
+isbnArr = []
 
 html_file = response.text
 soup = BeautifulSoup(html_file, 'html.parser')
 soup.prettify()
 # get titles of books from users to read shelf
-toRead_title = soup.findAll('td', 'field title', 'title')
-for book in toRead_title:
+
+for book in soup.findAll('td', 'field title', 'title'):
         titleArr.append(book.getText().replace('title','').strip())
 
 #  Get Books author 
-toRead_author =  soup.findAll('td', 'field author', 'title')
-for author in toRead_author:
+
+for author in  soup.findAll('td', 'field author', 'title'):
     authorArr.append(author.getText().replace('author','').replace('*', '').strip())
 
-toRead_rating =  soup.findAll('td', 'field avg_rating', 'title')
-for rating in toRead_rating:
+
+for rating in soup.findAll('td', 'field avg_rating', 'title'):
     ratingArr.append(rating.getText().replace('avg rating','').strip())
 
-toRead_isbn =  soup.findAll('td', 'field isbn', 'title')
-for isbn in toRead_isbn:
-    isbn.getText().replace('isbn', '').strip()
 
+for isbn in soup.findAll('td', 'field isbn', 'title'):
+    isbnArr.append(isbn.getText().replace('isbn', '').strip())
 
 
 for cover in soup.findAll( 'img'): 
@@ -37,15 +38,12 @@ for cover in soup.findAll( 'img'):
     if '.jpg' in str(c):
         coversArr.append(c)
 
-print(coversArr)
 
 ## TODO Pump out a random book 
 ## TODO Learn about making a front end for this 
 
-# for l in range(len(titleArr)):
-#     b = Book() 
-#     b.addDetails(titleArr[l], authorArr[l], ratingArr[l])
-#     bookList.append(b)
+for l in range(len(titleArr)):
+    b = Book() 
+    b.addDetails(titleArr[l], authorArr[l], ratingArr[l], isbnArr[l], coversArr[l])
+    bookList.append(b)
 
-
-# print(bookList[1].title + bookList[1].author)
